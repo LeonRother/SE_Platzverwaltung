1a4c9200ae3c0a7984196f4707720de86ad43ae5
Hpixpoke <hagenzagola@gmail.com>
2024-11-11 14:16:01 +0100

    Initial backend commit
    Includes primitive endpoints for buildings and floors.

---
+++ backend/Backend/Models/Floor.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+
+namespace Backend.Models
+{
+	[Table("Floors")]
+	public class Floor
+	{
+        [PrimaryKey, AutoIncrement]
+		[Column("ID")]
+        public int ID { get; set; }
+
+        [NotNull]
+        [Column("Label")]
+        public string? Label { get; set; }
+
+        // Foreign key
+        [NotNull]
+        [Column("BuildingID")]
+        public int BuildingID { get; set; }
+    }
+}
---
+++ backend/Backend/Services/BuildingService.cs
@@ -0,0 +1,18 @@
+﻿using Backend.Models;
+
+namespace Backend.Services
+{
+	public class BuildingService
+	{
+		private readonly DatabaseHandler _db;
+        public BuildingService(DatabaseHandler databaseHandler)
+        {
+            _db = databaseHandler;
+        }
+
+        public List<Building> GetAllBuildings()
+        {
+            return _db.Connection.Table<Building>().ToList();
+        }
+    }
+}
